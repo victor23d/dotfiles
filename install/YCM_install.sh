92e5aa534f9d431f7a07466a527699017c8b026e
victor23d <victor6742@outlook.com>
2018-10-07 14:04:53 +0000

    add go completer

--- install/YCM_install.sh
+++ install/YCM_install.sh
@@ -12,3 +12,20 @@ sudo apt-get install python-dev python3-dev
 ./install.py
 # ./install.py --js-completer --go-completer
 
+
+
+# install go
+# https://github.com/Valloric/YouCompleteMe/issues/3074
+cd ~/.vim/bundle
+git clone git@github.com:Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+python install.py --go-completer
+cd third_party/ycmd/third_party/
+rm -rf gocode
+git clone git@github.com:mdempsky/gocode.git
+cd gocode
+go mod init
+go build .
+
+
