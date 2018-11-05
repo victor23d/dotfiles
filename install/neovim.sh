ce3d9f4d2d635b01541bde24174dfef59a3c77a2
victor23d <cptdm2@gmail.com>
2018-11-05 12:55:41 +0000

    modified:   neovim.sh

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,3 +1,4 @@
-curl -Sslo /opt/nvim https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage
+curl -SsLo /opt/nvim https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage
+chmod 700 /opt/nvim
 ln -s /opt/nvim /usr/local/bin/
 
