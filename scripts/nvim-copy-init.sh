dc0e7551a782a0fb54a2cadad3e034c78c5dc1a8
victor23d <victor6742x@gmail.com>
2019-09-13 13:59:57 +0800

    modified:   nvim-copy-init.sh

--- scripts/nvim-copy-init.sh
+++ scripts/nvim-copy-init.sh
@@ -1,4 +1,5 @@
-cp -f ~/dotfiles/.vimrc /mnt/c/Users/Administrator/AppData/Local/nvim/init.vim
-cp -f ~/dotfiles/.vimrc /mnt/c/Users/Administrator/.ideavimrc
+cp -f ~/dotfiles/.vimrc 	/mnt/c/Users/Administrator/AppData/Local/nvim/init.vim
+cp -f ~/dotfiles/.vimrc 	/mnt/c/Users/Administrator/.vimrc
+cp -f ~/dotfiles/.ideavimrc 	/mnt/c/Users/Administrator/.ideavimrc
 # ln -sf ~/dotfiles/.vimrc-minimum /mnt/c/Users/Administrator/AppData/Local/nvim/init.vim
  echo "let g:python_host_prog='C:\python3\python3'" >> /mnt/c/Users/Administrator/AppData/Local/nvim/init.vim
