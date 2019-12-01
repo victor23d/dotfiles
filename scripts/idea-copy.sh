3260db4b25b37e6fe6ca264a89de23e9b67e1c0b
victor23d <victor6742x@gmail.com>
2019-12-01 11:41:14 +0800

    modified:   idea-copy.sh

--- scripts/idea-copy.sh
+++ scripts/idea-copy.sh
@@ -1,3 +1,3 @@
 set -ex
-cp -f ~/dotfiles/.idea/Cyan3.zip /mnt/c/Users/Administrator/
+cp -f ~/dotfiles/idea/Cyan3.zip /mnt/c/Users/Administrator/
 cp -f ~/dotfiles/.vimrc /mnt/c/Users/Administrator/.ideavimrc
